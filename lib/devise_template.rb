081a0e03b07325cb391aa5fe6c09eb0369256944
YutoYasunaga <yuto.yasunaga@gmail.com>
2023-09-02 01:08:38 +0900

    Initial commit

---
+++ lib/devise_template.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "devise_template/version"
+
+module DeviseTemplate
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/devise_template_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe DeviseTemplate do
+  it "has a version number" do
+    expect(DeviseTemplate::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
